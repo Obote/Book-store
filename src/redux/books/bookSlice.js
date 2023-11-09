52a633ee8d3472858938e9e14ed108e6e4d24568
Obote <obotedenis256@gmail.com>
2023-11-09 16:54:25 +0300

    Fix linters

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,30 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   books: [
     {
-      itemId: "item1",
-      title: "The Great Gatsby",
-      author: "John Smith",
-      category: "Fiction",
+      itemId: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
     },
     {
-      itemId: "item2",
-      title: "Anna Karenina",
-      author: "Leo Tolstoy",
-      category: "Fiction",
+      itemId: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
     },
     {
-      itemId: "item3",
-      title: "The Selfish Gene",
-      author: "Richard Dawkins",
-      category: "Nonfiction",
+      itemId: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
     },
   ],
 };
 
 const booksSlice = createSlice({
-  name: "books",
+  name: 'books',
   initialState,
   reducers: {
     addBook: (state, action) => {
@@ -32,7 +32,7 @@ const booksSlice = createSlice({
     },
     removeBook: (state, action) => {
       state.books = state.books.filter(
-        (book) => book.itemId !== action.payload
+        (book) => book.itemId !== action.payload,
       );
     },
   },
