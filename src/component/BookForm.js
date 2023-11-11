a6805406b60675b17210b2b275d1144cfc54451c
Obote <obotedenis256@gmail.com>
2023-11-11 19:33:47 +0300

    Correct import structure

--- src/component/BookForm.js
+++ src/component/BookForm.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBooks } from '../redux/books/bookSlice';
--- src/component/BookList.js
+++ src/component/BookList.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import { getBooks } from '../redux/books/bookSlice';
